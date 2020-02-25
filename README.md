1a53641d5da6fb28551884942a57d77067caeb6a
Matthew Donovan <matthewbryantdonovan@gmail.com>
2020-02-24 20:14:50 -0500

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# SuperClickyBros# SuperClickyBrosUltimate
---
+++ precache-manifest.3869b758d2bee6bc5f427341ed583ae0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40317c7f44ff0fa728dee5753efcf4b3",
+    "url": "/SuperClickyBros/index.html"
+  },
+  {
+    "revision": "a61037f919a4ae582fb6",
+    "url": "/SuperClickyBros/static/css/main.d6e1b1b6.chunk.css"
+  },
+  {
+    "revision": "a12d6d858896f310c680",
+    "url": "/SuperClickyBros/static/js/2.01debecb.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/SuperClickyBros/static/js/2.01debecb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a61037f919a4ae582fb6",
+    "url": "/SuperClickyBros/static/js/main.0fc389ad.chunk.js"
+  },
+  {
+    "revision": "5efa1872f73094d41f63",
+    "url": "/SuperClickyBros/static/js/runtime-main.6881278f.js"
+  }
+]);
